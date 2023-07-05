33173c672fce75342e36df40c46246738733678b
Andrei <surkov.andrew@gmail.com>
2023-07-05 16:07:56 +0200

    tic-tac-toe with simple AI

--- src/main/java/com/tictactoe/Field.java
+++ src/main/java/com/tictactoe/Field.java
@@ -1,9 +1,11 @@
 package com.tictactoe;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Field {
     private final Map<Integer, Sign> field;
@@ -26,10 +28,14 @@ public class Field {
     }
 
     public int getEmptyFieldIndex() {
-        return field.entrySet().stream()
+        List<Integer> ints = field.entrySet().stream()
                 .filter(e -> e.getValue() == Sign.EMPTY)
                 .map(Map.Entry::getKey)
-                .findFirst().orElse(-1);
+                .collect(Collectors.toList());
+        int index = (int) (Math.random() * ints.size());
+        if (ints.size() == 0) return -1;
+        return ints.get(index);
+
     }
 
     public List<Sign> getFieldData() {
@@ -53,10 +59,39 @@ public class Field {
 
         for (List<Integer> winPossibility : winPossibilities) {
             if (field.get(winPossibility.get(0)) == field.get(winPossibility.get(1))
-                && field.get(winPossibility.get(0)) == field.get(winPossibility.get(2))) {
+                    && field.get(winPossibility.get(0)) == field.get(winPossibility.get(2))) {
                 return field.get(winPossibility.get(0));
             }
         }
         return Sign.EMPTY;
+    }
+
+
+    public int checkPreWin() {
+        List<List<Integer>> winPossibilities = List.of(
+                List.of(0, 1, 2),
+                List.of(3, 4, 5),
+                List.of(6, 7, 8),
+                List.of(0, 3, 6),
+                List.of(1, 4, 7),
+                List.of(2, 5, 8),
+                List.of(0, 4, 8),
+                List.of(2, 4, 6)
+        );
+
+        for (List<Integer> winPossibility : winPossibilities) {
+            for (int i = 0; i < winPossibility.size(); i++) {
+                if (field.get(winPossibility.get(i)) != Sign.EMPTY) {
+                    if (field.get(winPossibility.get(1)) == field.get(winPossibility.get(2)) && field.get(winPossibility.get(0)) == Sign.EMPTY) {
+                        return winPossibility.get(0);
+                    } else if (field.get(winPossibility.get(0)) == field.get(winPossibility.get(1)) && field.get(winPossibility.get(2)) == Sign.EMPTY) {
+                        return winPossibility.get(2);
+                    } else if (field.get(winPossibility.get(0)) == field.get(winPossibility.get(2)) && field.get(winPossibility.get(1)) == Sign.EMPTY) {
+                        return winPossibility.get(1);
+                    }
+                }
+            }
+        }
+        return getEmptyFieldIndex();
     }
 }
